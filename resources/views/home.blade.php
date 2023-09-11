a3244f739656441f76de88525be525135dffde44
marcocampisi <marco.campisi@outlook.it>
2023-09-11 14:32:40 +0200

    Add cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,24 @@
 @extends('layouts.main')
 
+@section('page-title')
+    Movies DB
+@endsection
+
 @section('main-content')
+    <h1 class="my-4">Movies DB</h1>
+    <div class="row">
+        @foreach ($movies as $movie)
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $movie->title }}</h5>
+                        <p class="card-text">{{ $movie->original_title }}</p>
+                        <p class="card-text">{{ $movie->nationality }}</p>
+                        <p class="card-text">{{ $movie->date }}</p>
+                        <p class="card-text">{{ $movie->vote }}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
